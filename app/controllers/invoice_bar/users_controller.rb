ff6f12310dd9d9e066dadd269aeac530ea6edd8c
Josef Stribny <jstribny@redhat.com>
2015-10-07 20:30:41 +0200

    Remove inherited_resources dependency in users controller

--- app/controllers/invoice_bar/users_controller.rb
+++ app/controllers/invoice_bar/users_controller.rb
@@ -1,26 +1,34 @@
 module InvoiceBar
   class UsersController < InvoiceBar::ApplicationController
-    inherit_resources
-    respond_to :html, :json
-
-    before_filter :require_login, only: [:update]
-    before_filter :require_admin_rights, only: [:index, :edit, :destroy]
-
+    before_action :require_login, only: [:update]
+    before_action :require_admin_rights, only: [:index, :edit, :destroy]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     layout :set_layout
 
-    # Sign up
-    def new
-      @user = User.new
-      @user.build_address
+    # GET /users
+    # GET /users.json
+    def index
+      @users = InvoiceBar::User.all.page(params[:page])
+    end
 
-      respond_to do |format|
-        format.html
-        format.json { render json: @user }
-      end
+    # GET /users/1
+    # GET /users/1.json
+    def show
+    end
+
+    # This is sign up process.
+    # GET /users/new
+    def new
+      @user = InvoiceBar::User.new
+      @user.build_address
+    end
+
+    # GET /users/1/edit
+    def edit
     end
 
     def create
-      @user = User.new(params[:user])
+      @user = InvoiceBar::User.new(user_params)
 
       # First user is an administrator
       @user.administrator = true unless User.all.size > 0
@@ -36,11 +44,13 @@ module InvoiceBar
       end
     end
 
+    # PATCH/PUT /users/1
+    # PATCH/PUT /users/1.json
     def update
       params[:controller] = 'settings'
 
       @user = User.find(current_user.id)
-      @user.update_attributes(params[:user])
+      @user.update_attributes(user_params)
 
       respond_to do |format|
         if @user.save
@@ -53,17 +63,29 @@ module InvoiceBar
       end
     end
 
+    # DELETE /users/1
+    # DELETE /users/1.json
     def destroy
-      destroy! {}
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
 
-    protected
+    private
 
-      def collection
-        @users ||= end_of_association_chain.page(params[:page])
+      def set_user
+        @user = InvoiceBar::User.find(params[:id])
       end
 
-    private
+      def user_params
+        params.require(:user).permit(:name, :email, :ic, :phone, :web,
+                                     :administrator, :password, :crypted_password,
+                                     :salt, :remember_me_token, :remember_me_token_expires_at,
+                                     :reset_password_email_sent_at, :reset_password_token,
+                                     :reset_password_token_expires_at, :address_attributes)
+      end
 
       def set_layout
         case action_name
