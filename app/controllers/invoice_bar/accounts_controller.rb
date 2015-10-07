d805b743a1d5fa9538641f39e5c0b6990b9b15fc
Josef Stribny <jstribny@redhat.com>
2015-10-07 19:53:58 +0200

    Remove inherited_resources dependency in accounts controller

--- app/controllers/invoice_bar/accounts_controller.rb
+++ app/controllers/invoice_bar/accounts_controller.rb
@@ -1,30 +1,79 @@
 module InvoiceBar
   class AccountsController < InvoiceBar::ApplicationController
-    inherit_resources
-    respond_to :html, :json
+    before_action :require_login
+    before_action :fetch_currencies, only: [:new, :create, :edit, :update]
+    before_action :set_account, only: [:show, :edit, :update, :destroy]
 
-    before_filter :require_login
-    before_filter :fetch_currencies, only: [:new, :create, :edit, :update]
-
+    # GET /accounts
+    # GET /accounts.json
     def index
       @accounts = current_user.accounts.page(params[:page])
-
-      index! {}
     end
 
+    # GET /accounts/1
+    # GET /accounts/1.json
+    def show
+    end
+
+    # GET /accounts/new
+    def new
+      @account = InvoiceBar::Account.new
+    end
+
+    # GET /accounts/1/edit
+    def edit
+    end
+
+    # account /accounts
+    # account /accounts.json
     def create
-      @account = Account.new(params[:account])
+      @account = InvoiceBar::Account.new(account_params)
       current_user.accounts << @account
 
-      create! {}
+      respond_to do |format|
+        if @account.save
+          format.html { redirect_to @account, notice: 'Account was successfully created.' }
+          format.json { render :show, status: :created, location: @account }
+        else
+          format.html { render :new }
+          format.json { render json: @account.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
+    # PATCH/PUT /accounts/1
+    # PATCH/PUT /accounts/1.json
     def update
-      update! {}
+      respond_to do |format|
+        if @account.update(account_params)
+          format.html { redirect_to @account, notice: 'Account was successfully updated.' }
+          format.json { render :show, status: :ok, location: @account }
+        else
+          format.html { render :edit }
+          format.json { render json: @account.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
+    # DELETE /accounts/1
+    # DELETE /accounts/1.json
     def destroy
-      destroy! {}
+      @account.destroy
+      respond_to do |format|
+        format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
+
+    private
+
+      def set_account
+        @account = InvoiceBar::Account.find(params[:id])
+      end
+
+      def account_params
+        params.require(:account).permit(:amount, :bank_account_number, :iban,
+                                        :name, :swift, :user_id, :currency_id)
+      end
   end
 end
