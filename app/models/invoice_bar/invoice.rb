17a361a49a990c25363fa84f07bfc4cb06b1836c
Josef Strzibny <strzibny@strzibny.name>
2016-03-11 10:22:12 +0100

    Delete unused #to_pdf

--- app/models/invoice_bar/invoice.rb
+++ app/models/invoice_bar/invoice.rb
@@ -35,53 +35,6 @@ module InvoiceBar
       self.sent = true
     end
 
-    def to_pdf
-      require 'invoice_printer'
-      printable_items = []
-
-      items.each do |item|
-        printable_items << InvoicePrinter::Document::Item.new(
-          name: item.name,
-          quantity: item.number,
-          unit: item.unit,
-          price: item.price,
-          amount: item.amount
-        )
-      end
-
-      invoice = InvoicePrinter::Document.new(
-        number: number,
-        provider_name: user_name,
-        provider_ic: user_ic,
-        provider_street: user_street,
-        provider_street_number: user_street_number,
-        provider_postcode: user_postcode,
-        provider_city: user_city,
-        provider_city_part: user_city_part,
-        provider_extra_address_line: user_extra_address_line,
-        purchaser_name: contact_name,
-        purchaser_ic: contact_ic,
-        purchaser_dic: contact_dic,
-        purchaser_street: address_street,
-        purchaser_street_number: address_street_number,
-        purchaser_postcode: address_postcode,
-        purchaser_city: address_city,
-        purchaser_city_part: address_city_part,
-        purchaser_extra_address_line: address_extra_address_line,
-        issue_date: issue_date.to_s,
-        due_date: due_date.to_s,
-        total: amount.to_s,
-        bank_account_number: account_bank_account_number,
-        account_iban: account_iban,
-        account_swift: account_swift,
-        items: printable_items
-      )
-
-      render = InvoicePrinter.render(
-        document: invoice
-      )
-    end
-
     private
 
       # Validates uniqueness of a number for current user.
