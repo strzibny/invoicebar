d418f37f10138c1d8e656a150bbc5b2388de216e
Josef Strzibny <strzibny@strzibny.name>
2016-12-19 14:58:26 +0100

    Test create first invoice

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -83,4 +83,5 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
   config.include Features::SessionHelpers, type: :feature
+  config.include ActiveSupport::Testing::TimeHelpers
 end
--- spec/stories/story_01_spec.rb
+++ spec/stories/story_01_spec.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 
 RSpec.feature 'User story 1' do
   scenario 'story 1' do
+    travel_to Time.new(2016, 01, 02, 1, 04, 44)
+
     # Set up currencies
     create(:invoice_bar_currency, name: 'CZK', symbol: 'Kc')
     create(:invoice_bar_currency, name: 'EUR', symbol: 'E')
@@ -36,8 +38,11 @@ RSpec.feature 'User story 1' do
     # Setting the last invoice number
     click_link t('invoice_bar.navbar.settings')
     click_link t('navigation.sequences_settings')
+
+    # TODO: count by year/month/day
     fill_in t('attributes.user.preferences.issued_invoice_sequence'), with: 'VF%Y%m'
-    fill_in t('attributes.user.preferences.last_issued_invoice'), with: 'VF201512050010'
+    fill_in t('attributes.user.preferences.last_issued_invoice'), with: 'VF2015120010'
+    click_button t('buttons.save')
 
     # Set up bank accounts
     create_account name: 'Account 1',
@@ -54,11 +59,18 @@ RSpec.feature 'User story 1' do
                    currency: 'CZK',
                    amount: '118000'
 
-
     # Generating first January invoice
+    create_invoice contact: client_a,
+                   account_name: 'Account 2',
+                   issue_date: '2016-01-02',
+                   due_date: '2016-01-15',
+                   note: 'Podnikatel je zapsán do živnostenského rejstříku',
+                   items: [
+                    build(:invoice_bar_item)
+                   ]
 
-    expect(page).to have_content('VF201601020001')
-    click_button t('buttons.save')
+    expect(page).to have_content('VF2016010001')
+    #click_button t('buttons.create')
 
     # Log out
     sign_out
@@ -85,14 +97,46 @@ RSpec.feature 'User story 1' do
   end
 
   def client_a
-    build(:invoice_bar_contact, name: 'Client A, s.r.o.')
+    build(:invoice_bar_contact, name: 'Client A, s.r.o.', address: build(:invoice_bar_address))
   end
 
   def client_b
-    build(:invoice_bar_contact, name: 'Client B, a.s.')
+    build(:invoice_bar_contact, name: 'Client B, a.s.', address: build(:invoice_bar_address))
   end
 
   def client_c
-    build(:invoice_bar_contact, name: 'Client C')
+    build(:invoice_bar_contact, name: 'Client C', address: build(:invoice_bar_address))
+  end
+
+  def create_invoice(contact:,
+                     account_name:,
+                     issue_date:,
+                     due_date:,
+                     note:,
+                     items:)
+    click_link t('invoice_bar.navbar.invoices')
+    click_link t('links.new_invoice')
+    fill_in t('attributes.invoice.contact_name'), with: contact.name
+    fill_in t('attributes.invoice.contact_tax_id'), with: contact.tax_id
+    fill_in t('attributes.invoice.contact_tax_id2'), with: contact.tax_id2
+    fill_in t('attributes.address.street'), with: contact.street
+    fill_in t('attributes.address.street_number'), with: contact.street_number
+    fill_in t('attributes.address.postcode'), with: contact.postcode
+    fill_in t('attributes.address.city'), with: contact.city
+    fill_in t('attributes.address.city_part'), with: contact.city_part
+    fill_in t('attributes.address.extra_address_line'), with: contact.extra_address_line
+
+    select account_name, from: t('attributes.invoice.account_id')
+    fill_in t('attributes.invoice.issue_date'), with: issue_date
+    fill_in t('attributes.invoice.due_date'), with: due_date
+    fill_in t('attributes.invoice.note'), with: note
+
+    items.each do |item|
+      fill_in t('attributes.item.name'), with: item.name
+      fill_in t('attributes.item.number'), with: item.number
+      fill_in t('attributes.item.unit'), with: item.unit
+      fill_in t('attributes.item.price'), with: item.price
+    end
+    click_button t('buttons.save')
   end
 end
